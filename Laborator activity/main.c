cf2176ee0f275a26bcb1df45bc5def5b6f4d30b7
sabauandrei98 <sabau_andrei1998@yahoo.com>
2018-03-13 14:33:26 +0100

    Update

---
+++ Laborator activity/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getTriangleElement(int line, int col)
+{
+    for(int i = 1; i <= 3; i++)
+    {
+    }
+
+    return -1;
+}
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
